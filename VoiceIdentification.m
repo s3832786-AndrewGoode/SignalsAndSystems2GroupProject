82334a3945d644ee91fc510527bdd2c39c1842d5
Rory Ribarits s3840280 <s3840280@student.rmit.edu.au>
2024-05-16 09:44:13 +1000

    Included samples we will load from dataset

--- VoiceIdentification.m
+++ VoiceIdentification.m
@@ -8,3 +8,23 @@ close all;
 clc;
 
 %% Load data
+% The VCTK Corpus dataset was used as the unknown class to detect an
+% unautorised person. The group members recorded themselves saying the same
+% sentences to act as the autorised people.
+
+% IDs taken from VCTK Corpus dataset:
+%   p244 F
+%   p286 M
+%   p326 M
+%   p335 F
+%   p361 F
+%   p374 M
+
+% Sentences taken from VCTK Corpus dataset:
+%   006 - 024
+
+%% Convert data to MEL spectrogram
+
+[audioIn,fs] = audioread('./Voice Dataset/p374/p374_007_mic2.flac');
+
+S = melSpectrogram(audioIn,fs);
