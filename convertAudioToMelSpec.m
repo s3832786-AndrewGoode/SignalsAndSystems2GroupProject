7a3f4a5b2c63192a916918040a586d1d3b0debb3
Rory Ribarits s3840280 <s3840280@student.rmit.edu.au>
2024-05-17 12:29:10 +1000

    Adjusted function to read dataset and output only MEL spectrogram

--- convertAudioToMelSpec.m
+++ convertAudioToMelSpec.m
@@ -1,7 +1,7 @@
-function convertAudioToMelSpec(audioDir)
+function convertAudioToMelSpec(audioDir, person)
     % Need to Adjust path
     % audioDir = './';
-    outputDir = './mel_spectrograms/';
+    outputDir = ['./mel_spectrograms/', person];
     
     % Create output dir
     if ~exist(outputDir, 'dir')
@@ -10,25 +10,34 @@ function convertAudioToMelSpec(audioDir)
     
     % Loop through each audio file
     for i = 1:19
-       % Construct File
-       audioFile = fullfile(audioDir, ['audio',num2str(i),'.wav']);
-       
-       % Reading the Audio File 
-       [y,fs] = audioread(audioFile);
-       
-       % Parameters For the Mel Spectrogram
-       % Window Legth
-       winLength = round(0.025 * fs);
-       hopLength = round(0.010 * fs);
-       numBands = 40;
-       
-       % Compute the Mel spectrogram
-        melSpec = melSpectrogram(y, fs, 'WindowLength', winLength,'OverlapLength', winLength - hopLength, 'NumBands', numBands);
+        if i + 5 < 10
+            % Construct File
+            audioFile = fullfile(audioDir, [person, '_00', num2str(i + 5), '_mic1.flac']);
+        else
+            % Construct File
+            audioFile = fullfile(audioDir, [person, '_0', num2str(i + 5), '_mic1.flac']);
+        end
         
-        figure
-        melSpec(y, fs, 'WindowLength', winLength,'OverlapLength', winLength - hopLength, 'NumBands', numBands);
+        % Reading the Audio File 
+        [y,fs] = audioread(audioFile);
+       
+        % Parameters For the Mel Spectrogram
+        % Window Length
+        winLength = round(0.025 * fs);
+        hopLength = round(0.010 * fs);
+        numBands = 40;
+        
+        figure;
+        % Compute the Mel spectrogram
+        melSpectrogram(y, fs, 'WindowLength', winLength,'OverlapLength', winLength - hopLength, 'NumBands', numBands);
+        set(gca, 'XTick', [], 'YTick', [], 'XColor', 'none', 'YColor', 'none');
+        colorbar('off');       
+        set(gca, 'LooseInset', get(gca, 'TightInset'));
+        set(gca, 'Position', [0 0 1 1], 'Units', 'normalized');
         
         outputFile = fullfile(outputDir, ['melSpectrogram', num2str(i),'.png']);
         saveas(gcf, outputFile);
+        
+        close gcf;
     end
 end
