f7c7f2ac28e22b6d4a557741059151e86d440985
Rory Ribarits s3840280 <s3840280@student.rmit.edu.au>
2024-05-17 15:39:56 +1000

    Updated colormap of spectrograms

--- convertAudioToMelSpec.m
+++ convertAudioToMelSpec.m
@@ -35,6 +35,7 @@ function convertAudioToMelSpec(audioDir, person)
         figure;
         % Compute the Mel spectrogram
         melSpectrogram(y, fs, 'WindowLength', winLength,'OverlapLength', winLength - hopLength, 'NumBands', numBands);
+        colormap(hot(256));
         set(gca, 'XTick', [], 'YTick', [], 'XColor', 'none', 'YColor', 'none');
         colorbar('off');       
         set(gca, 'LooseInset', get(gca, 'TightInset'));
