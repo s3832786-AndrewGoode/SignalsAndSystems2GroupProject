092314b62e33b0872d5f889484529cd6d7e52aaa
Rory Ribarits s3840280 <s3840280@student.rmit.edu.au>
2024-05-21 10:59:28 +1000

    Implemented voice identification model into GUI

---
+++ convertSingleAudioToMelSpec.m
@@ -0,0 +1,35 @@
+function convertSingleAudioToMelSpec(audioDir, person)
+    outputDir = ['./Testing/', person];
+    
+    % Create output dir
+    if ~exist(outputDir, 'dir')
+        mkdir(outputDir);
+    end
+    
+    try
+        % Reading the Audio File 
+        [y,fs] = audioread(audioDir);
+    catch
+        warning(['File ', audioDir, ' not found.']);
+    end
+
+    % Parameters For the Mel Spectrogram
+    % Window Length
+    winLength = round(0.025 * fs);
+    hopLength = round(0.010 * fs);
+    numBands = 40;
+
+    figure;
+    % Compute the Mel spectrogram
+    melSpectrogram(y, fs, 'WindowLength', winLength,'OverlapLength', winLength - hopLength, 'NumBands', numBands);
+    colormap(hot(256));
+    set(gca, 'XTick', [], 'YTick', [], 'XColor', 'none', 'YColor', 'none');
+    colorbar('off');       
+    set(gca, 'LooseInset', get(gca, 'TightInset'));
+    set(gca, 'Position', [0 0 1 1], 'Units', 'normalized');
+
+    outputFile = fullfile(outputDir, ['melSpectrogram', num2str(i),'.png']);
+    saveas(gcf, outputFile);
+
+    close gcf;
+end
